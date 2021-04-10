0404ae1ee63103b91ca3bea49fd083cd6459b938
UcGeorge <petegeorge20005@gmail.com>
2021-04-10 17:50:18 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 api = Api()
 db = SQLAlchemy()
+
+if True:
+    from model import (chapter_model, novel_model, user_model)

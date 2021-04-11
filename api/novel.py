d658b2a8f12cd172e2f48275fb719428f275f097
UcGeorge <petegeorge20005@gmail.com>
2021-04-11 12:55:28 +0100

    Modified some endpoints

--- api/novel.py
+++ api/novel.py
@@ -1,39 +1,36 @@
-from app import db
+from app import db, helper
 from model.novel_model import novel
 from flask_restful import Resource, abort, marshal_with, fields, reqparse
 
 getFields = {
-    'id': fields.Integer,
     'name': fields.String
 }
 
 putNovelArgs = reqparse.RequestParser()
-putNovelArgs.add_argument('id', type=int,
-                          help='id of novel is required', required=True)
 putNovelArgs.add_argument('name', type=str,
                           help='Name of novel is required', required=True)
 
 getNovelArgs = reqparse.RequestParser()
-getNovelArgs.add_argument('id', type=int,
-                          help='id of novel is required')
+getNovelArgs.add_argument('name', type=str,
+                          help='name of novel is required')
 
 
 class Novel(Resource):
 
-    def get_single(self, id):
-        result = novel.query.get(id)
+    def get_single(self, name: str):
+        result = novel.query.get(helper.str_to_id(name))
         if not result:
-            abort(404, message=f'novel with id: {id} does not exist')
+            abort(404, message=f'{name} does not exist')
         return result
 
     @marshal_with(getFields)
     def get(self):
         args = getNovelArgs.parse_args()
 
-        if args['id'] is not None:
-            return self.get_single(args['id'])
+        if args['name'] is not None:
+            return self.get_single(args['name'])
 
-        result = novel.query.order_by(novel.id).all()
+        result = novel.query.order_by(novel.name).all()
         if result == [] or result is None:
             abort(404, message='There are no novels')
         return result
@@ -42,12 +39,12 @@ class Novel(Resource):
     def put(self):
 
         args = putNovelArgs.parse_args()
-        the_novel = novel.query.get(args['id'])
+        the_novel = novel.query.get(helper.str_to_id(args['name']))
 
         if the_novel:
-            abort(401, message=f"novel with id: {args['id']} already exists")
+            abort(401, message=f"{args['name']} already exists")
 
-        new_novel = novel(id=args['id'], name=args['name'])
+        new_novel = novel(name=helper.str_to_id(args['name']))
         db.session.add(new_novel)
         db.session.commit()
-        return the_novel, 201
+        return args, 201
--- api/reads.py
+++ api/reads.py
@@ -5,24 +5,23 @@ from model.novel_model import novel
 from app import db, helper
 
 getReadsFields = {
-    'id': fields.Integer,
     'name': fields.String
 }
 
 putReadArgs = reqparse.RequestParser()
-putReadArgs.add_argument('novel_id', type=int,
-                         help='id of novel is required', required=True)
+putReadArgs.add_argument('novel_name', type=str,
+                         help='name of novel is required', required=True)
 
 
 class Reads(Resource):
 
     @marshal_with(getReadsFields)
     def get(self, username: str):
-        result = user.query.get(username.lower())
+        result = user.query.get(helper.str_to_id(username))
         if not result:
             abort(404, message=f'{username} does not exist')
 
-        sel = user_novel.select().where(user_novel.c.username == username)
+        sel = user_novel.select().where(user_novel.c.username == helper.str_to_id(username))
 
         sql_res = db.engine.execute(sel)
 
@@ -31,27 +30,30 @@ class Reads(Resource):
         if user_novels == []:
             abort(404, message=f'User has no novels')
         else:
-            result = [novel.query.get(x['novel_id']) for x in user_novels]
+            result = [novel.query.get(helper.str_to_id(
+                x['novel_name'])) for x in user_novels]
 
         return result
 
     @marshal_with(getReadsFields)
     def put(self, username: str):
         args = putReadArgs.parse_args()
-        the_user = user.query.get(username.lower())
-        the_novel = novel.query.get(args['novel_id'])
+        the_user = user.query.get(helper.str_to_id(username))
+        the_novel = novel.query.get(helper.str_to_id(args['novel_name']))
         if not (the_user and the_novel):
             abort(
-                409, message=f"{username if not the_user else args['novel_id']} doesn't exist")
+                409, message=f"{username if not the_user else args['novel_name']} doesn't exist")
 
-        sel = user_novel.select().where(user_novel.c.username == username)
+        sel = user_novel.select().where(user_novel.c.username == helper.str_to_id(username))
         sql_res = db.engine.execute(sel)
         user_novels = helper.parse_ResultProxy(sql_res)
 
-        if the_novel.id in [x['novel_id'] for x in user_novels]:
+        if the_novel.name in [x['novel_name'] for x in user_novels]:
             abort(401, message=f"This novel is already part of the user's collection")
 
         ins = user_novel.insert().values(
-            username=username, novel_id=args['novel_id'])
+            username=helper.str_to_id(username),
+            novel_name=helper.str_to_id(args['novel_name'])
+        )
         db.engine.execute(ins)
         return the_novel, 201

3c52acc15dea597103a892a70a72e630daa9117a
UcGeorge <petegeorge20005@gmail.com>
2021-04-11 10:21:38 +0100

    Create helper.py

---
+++ app/helper.py
@@ -0,0 +1,10 @@
+
+def parse_ResultProxy(resultproxy):
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+    return a

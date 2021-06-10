5eb8dffc87e37b06b85068abf361bf041bb48deb
UcGeorge <petegeorge20005@gmail.com>
2021-06-10 14:02:25 +0100

    Updated a few models

--- main.py
+++ main.py
@@ -22,5 +22,14 @@ api.add_resource(chapter.Chapter, '/chapter/<string:novel_name>')
 api.init_app(app)
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- model/novel_model.py
+++ model/novel_model.py
@@ -3,6 +3,7 @@ from app import db
 
 class novel(db.Model):
     name = db.Column(db.String(80), primary_key=True)
+    cover_image = db.Column(db.String(80))
     chapters = db.relationship('chapter', backref='novel', lazy=True)
 
     def __repr__(self) -> str:

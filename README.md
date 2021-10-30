d115c08c2e0c405c8a954628508bf256d3463faa
UcGeorge <petegeorge20005@gmail.com>
2021-10-30 20:13:44 +0100

    Switch to Node.js

--- README.md
+++
@@ -1,3 +0,0 @@
-# PGL-API
-
-Backend for the <a href='https://github.com/UcGeorge/profound_god_library'>Profound God Library</a> application
--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-
-api = Api()
-db = SQLAlchemy()
-
-if True:
-    from model import (chapter_model, novel_model, user_model)

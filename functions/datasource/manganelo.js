b2097e41416b7e714f93ea001a6dd409400d0d74
UcGeorge <petegeorge20005@gmail.com>
2021-11-05 14:00:43 +0100

    Manganelo - details

--- functions/datasource/manganelo.js
+++ functions/datasource/manganelo.js
@@ -3,8 +3,12 @@ const fetch = require('isomorphic-fetch');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-var sourceName = "Manganelo";
-var searchLink = "https://manganato.com/search/story/"
+const sourceName = "Manganelo";
+const searchLink = "https://manganato.com/search/story/"
+const chapterItemSelector = "body > div.body-site > div.container.container-main > div.container-main-left > div.panel-story-chapter-list > ul > li"
+const descriptionSelector = "#panel-story-info-description";
+const statusSelector = "body > div.body-site > div.container.container-main > div.container-main-left > div.panel-story-info > div.story-info-right > table > tbody > tr:nth-child(3) > td.table-value"
+const ratingSelector = "#rate_row_cmd > em > em:nth-child(2) > em > em:nth-child(1)"
 
 exports.search = async(searctTerm) => {
     try {
@@ -14,7 +18,7 @@ exports.search = async(searctTerm) => {
         const text = await response.text();
         const dom = new JSDOM(text);
         var result = [];
-        var items = dom.window.document.querySelectorAll("div.search-story-item").forEach(_parseItem);
+        dom.window.document.querySelectorAll("div.search-story-item").forEach(_parseItem);
 
         function _parseItem(value, index, array) {
             const _dom = new JSDOM(value.innerHTML);
@@ -39,3 +43,42 @@ exports.search = async(searctTerm) => {
         throw e;
     }
 };
+
+exports.details = async(mangaLink) => {
+    try {
+        functions.logger.info({ source: sourceName, function: "details", param: mangaLink });
+        // Main function body
+        const response = await fetch(mangaLink);
+        const text = await response.text();
+        const dom = new JSDOM(text);
+        var result = {
+            "status": "",
+            "rating": 0,
+            "description": "",
+            "chapter_count": 0,
+            "chapters": [],
+        };
+        dom.window.document.querySelectorAll(chapterItemSelector).forEach(_getChapters);
+        result.description = dom.window.document.querySelector(descriptionSelector).textContent.replace('\nDescription :\n', "")
+        result.status = dom.window.document.querySelector(ratingSelector).textContent
+
+        function _getChapters(value, index, array) {
+            var chapter = {
+                "index": index,
+                "name": "",
+                "link": ""
+            };
+            const _dom = new JSDOM(value.innerHTML);
+            chapter.name = _dom.window.document.querySelector("a").textContent;
+            chapter.link = _dom.window.document.querySelector("a").getAttribute('href');
+
+            result.chapters.push(chapter);
+            result.chapter_count++;
+        }
+        // End Main function body
+        return result;
+    } catch (e) {
+        functions.logger.error({ source: sourceName, function: "search", error: e.message });
+        throw e;
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -16,3 +16,15 @@ exports.search = functions.https.onRequest(async(request, response) => {
         response.status(400).json({ "message": error.message });
     }
 });
+
+exports.details = functions.https.onRequest(async(request, response) => {
+    var queryBody = request.body;
+    functions.logger.info("Details", { body: queryBody });
+    try {
+        var result = await datasources[queryBody.source].details(queryBody.link);
+        response.status(200).send(result);
+    } catch (error) {
+        functions.logger.error(error.message);
+        response.status(400).json({ "message": error.message });
+    }
+});
